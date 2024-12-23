8227d3da195aa5a1a5dad5503ca42e2c7191b045
Shrikant Verma <shrikant.verma@mandalsolution.com>
2024-12-24 03:36:09 +0530

    update color

--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -100,24 +100,27 @@ function Career() {
       <div className="w-full md:w-2/3 p-4 overflow-auto">
       <div className="flex justify-between items-center mb-4">
           <h2 className="text-lg font-semibold">Upload Your Resume For Perfect Match</h2>
-          <button className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700">Upload Resume</button>
+          <button className="bg-primary text-white px-4 py-2 rounded hover:bg-green-700">Upload Resume</button>
         </div>
         <div className="flex gap-2 mb-4">
           <input
             type="text"
             placeholder="Search Job"
-            className="flex-1 border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-green-500"
+            className="flex-1 border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:bg-primary"
           />
           <input
             type="text"
             placeholder="Search Location"
-            className="flex-1 border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-green-500"
+            className="flex-1 border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:bg-primary"
           />
-          <button className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700">🔍</button>
+          <button className="text-white px-4 py-2 rounded hover:bg-primary">🔍</button>
         </div>
         {selectedJob ? (
           <div className="bg-white p-4 rounded-lg shadow">
             <h3 className="text-lg font-semibold mb-2">{selectedJob.jobTitle || "N/A"}</h3>
+            <button className="bg-primary text-white px-4 py-2 rounded hover:bg-primary mb-4">
+            Apply Now
+          </button>
             <p className="text-sm text-gray-500">{selectedJob.company || "N/A"}</p>
             <div className="flex flex-wrap gap-2 my-2">
               <span className="bg-gray-200 text-sm px-2 py-1 rounded">{selectedJob.location || "N/A"}</span>
