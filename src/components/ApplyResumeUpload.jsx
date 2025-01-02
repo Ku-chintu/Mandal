06df03eb5a15a708cc211d66fe400533dfbd3e08
Shrikant Verma <shrikant.verma@mandalsolution.com>
2025-01-02 13:57:03 +0530

    accordion images fixed

--- src/components/ApplyResumeUpload.jsx
+++ src/components/ApplyResumeUpload.jsx
@@ -1,6 +1,5 @@
 import React, { useRef } from "react";
 import { ToastContainer, toast } from "react-toastify";
-//import "react-toastify/dist/ReactToastify.css";
 
 import service from "../service";
 import utility from "../utility";
@@ -9,7 +8,6 @@ const ApplyResumeUpload = ({ jobId }) => {
   const fileInputRef = useRef(null);
 
   const handleUploadClick = () => {
-    // Programmatically trigger the hidden file input
     fileInputRef.current.click();
   };
 
@@ -17,51 +15,51 @@ const ApplyResumeUpload = ({ jobId }) => {
     const file = event.target.files[0];
     if (file) {
       console.log("Uploaded file:", file.name);
-      // Add further logic to handle the uploaded file
-
       try {
-            // Convert the resume file to base64
-            const base64Content = await utility.convertFileToBase64(file);
-            const validExtensions = [".doc", ".docx", ".pdf", ".rtf"];
-            const fileExtension = file.name.slice(file.name.lastIndexOf(".")).toLowerCase();
-      
-            // Construct the resume object
-            const resumeData = {
-              name: file.name,
-              content: base64Content,
-              length: file.size,
-              mediaType: `.${file.name.split(".").pop()}`,
-            };
-      
-            // Create the payload to send to the API
-            const formData = new FormData();
-            formData.append("resumes", JSON.stringify(resumeData));
-      
-            // Mock the POST request to API with progress
-            const config = {
-              headers: { "Content-Type": "application/json" },
-              jobId: jobId
-            };
-            
-            if (validExtensions.includes(fileExtension)) 
-            {
-                const response = await service.requestApi.resumeUpload(
-                  resumeData,
-                  config
-                );
-                if (response.status === 200) {
-                    toast.success(`Your application is successfully submitted!`);
-                } else {
-                    toast.error(`Failed to apply, please try again!`);
-                }
-            } else
-              {
-                toast.error(`Invalid file type. Supported file types are doc, docx, pdf, rtf.`);
-              }
-          } catch (error) {
-            toast.error(`Failed to apply!`);
-            console.error(error);
+        const base64Content = await utility.convertFileToBase64(file);
+        const validExtensions = [".doc", ".docx", ".pdf", ".rtf"];
+        const fileExtension = file.name
+          .slice(file.name.lastIndexOf("."))
+          .toLowerCase();
+
+        const resumeData = {
+          name: file.name,
+          content: base64Content,
+          length: file.size,
+          mediaType: `.${file.name.split(".").pop()}`,
+        };
+
+        const formData = new FormData();
+        formData.append("resumes", JSON.stringify(resumeData));
+
+        const config = {
+          headers: { "Content-Type": "application/json" },
+          jobId: jobId,
+        };
+
+        if (validExtensions.includes(fileExtension)) {
+          const response = await service.requestApi.resumeUpload(
+            resumeData,
+            config
+          );
+          if (response.status === 200) {
+            toast.success(`Your application is successfully submitted!`);
+          } else {
+            toast.error(`Failed to apply, please try again!`);
           }
+        } else {
+          toast.error(
+            `Invalid file type. Supported file types are doc, docx, pdf, rtf.`
+          );
+        }
+      } catch (error) {
+        toast.error(`Failed to apply!`);
+        console.error(error);
+      } finally {
+        fileInputRef.current.value = "";
+      }
+    } else {
+      toast.error("No file selected. Please choose a valid resume.");
     }
   };
 
@@ -73,7 +71,7 @@ const ApplyResumeUpload = ({ jobId }) => {
         onClick={handleUploadClick}
       >
         Apply Now
-        <p style={{fontSize:'4px'}} >pdf, doc, docx, rtf</p>
+        <p style={{ fontSize: "4px" }}>pdf, doc, docx, rtf</p>
       </button>
       <input
         type="file"
@@ -82,7 +80,6 @@ const ApplyResumeUpload = ({ jobId }) => {
         onChange={handleFileChange}
         accept=".pdf,.doc,.docx,.rtf"
       />
-      {/* <p className="font-normal text-xs">Accepted formats: PDF, DOC, DOCX</p> */}
       <ToastContainer position="top-right" autoClose={3000} />
     </div>
   );
