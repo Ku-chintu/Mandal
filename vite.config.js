2032ad0ef7460ac6c29bac4e1cf63c213a5c1263
Ku-chintu <kumar.chintu1987@gmail.com>
2025-07-14 20:58:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-
-// vite.config.js
-export default {
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    strictPort: true
-  }
-}
